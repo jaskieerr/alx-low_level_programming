7234962ea6c9134c53d70cec577deccd9daf755a
jaskieerr <nizar.zairat@gmail.com>
2023-06-15 06:36:52 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - a function that prints a string using the printf function
+*
+* Return: always 0 (success)
+*/
+
+
+int	main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,/n");
+	return (0);
+}

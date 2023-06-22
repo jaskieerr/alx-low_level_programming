a91516711821e0f3c8f26ed9cae7845ef534aa07
jaskieerr <nizar.zairat@gmail.com>
2023-06-22 13:27:49 -0700

    printing numbas

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbas from 0 to 10 baby
+ *
+ * return: void
+ *
+*/
+
+void print_numbers(void)
+
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}

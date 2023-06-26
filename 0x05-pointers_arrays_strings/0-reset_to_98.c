6aae9446c7195e0d0bf3757bab013a3c820c0370
jaskieerr <nizar.zairat@gmail.com>
2023-06-26 13:50:25 -0700

    BISSMILAH

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - It takes a pointer to an int as parameter and update its value
+ * to 98.
+ * @n: pointerr to integer.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+Thsis the alx 0x05. C - Pointers, arrays and strings project
+ In this directory i will be uploading my work

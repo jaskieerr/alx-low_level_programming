6af31aec5d01ab793f179c7972d9ba95ba587dfa
jaskieerr <nizar.zairat@gmail.com>
2023-06-29 08:51:41 -0700

    finale

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* print_buffer - it prints the buffa
+ * @b: the address memory to be printed
+ * @size: the size of memory to be printed
+ */
+void print_buffer(char *b, int size)
+{
+	int offset, byte_index, char_index;
+
+	for (offset = 0; offset < size; offset += 10)
+	{
+		printf("%08x: ", offset);
+		for (byte_index = 0; byte_index < 10; byte_index++)
+		{
+			(offset + byte_index < size) ? printf("%02x", b[offset + byte_index])
+				: printf("  ");
+			(byte_index % 2) ? printf(" ") : 0;
+		}
+		for (char_index = 0; char_index < 10; char_index++)
+			(offset + char_index < size) ? printf("%c", (b[offset + char_index] >= 32 &&
+				b[offset + char_index] <= 126) ? b[offset + char_index] : '.') : 0;
+
+		printf("\n");
+	}
+
+	(size <= 0) ? printf("\n") : 0;
+}

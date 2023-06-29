7ea576c2633d788fc8b46bc5e94b45722f5f4e4f
jaskieerr <nizar.zairat@gmail.com>
2023-06-29 07:38:54 -0700

    chestt

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 -  it encodes string via rot13
+ * @s: the string to be encoded
+ * Return: the Pointer to the encoded string s
+ */
+char *rot13(char *s)
+{
+	int i, g;
+	char *a = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *b = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i]; i++)
+	{
+		for (g = 0; a[g]; g++)
+		{
+			if (s[i] == a[g])
+			{
+				s[i] = b[g];
+				break;
+			}
+		}
+	}
+
+i	return (s);
+}

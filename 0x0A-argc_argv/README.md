1cc16b70801ce43809ad1c818e6a72cbb8e46e92
jaskieerr <nizar.zairat@gmail.com>
2023-07-07 04:11:59 -0700

    say my name

---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+Hi again
+	THIS is the 0x0A-argc_argv project in this project i will be uploding stuff
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -a function that prints name of program
+ * @argc: the numba of arguments
+ * @argv: the array of arguments
+ * Return: 0
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

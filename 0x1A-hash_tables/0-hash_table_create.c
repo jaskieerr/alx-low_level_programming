af936887af7907df590851594a8b9fe37ff163fa
jaskieerr <nizar.zairat@gmail.com>
2023-11-03 16:19:19 -0700

    money

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,27 +3,24 @@
 /**
  * hash_table_create - it creates a hash table
  * @size: array sizu
- * Return: pointer to the newly created hash table
+ * Return: pointer to the created hash_table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *the_ht;
-	hash_node_t **the_arr;
+	hash_table_t *hash_tt;
 	unsigned long int i;
 
-	the_ht = malloc(sizeof(hash_table_t));
-	if (the_ht == NULL)
+	hash_tt = malloc(sizeof(hash_table_t));
+	if (hash_tt == NULL)
 		return (NULL);
-
-	the_arr = malloc(sizeof(hash_node_t *) * size);
-	if (the_arr == NULL)
+	hash_tt->size = size;
+	hash_tt->array = malloc(size * sizeof(hash_node_t *));
+	if (hash_tt->array == NULL)
+	{
+		free(hash_tt);
 		return (NULL);
-
+	}
 	for (i = 0; i < size; i++)
-		the_arr[i] = NULL;
-
-	the_ht->the_arr = the_arr;
-	the_ht->size = size;
-
-	return (the_ht);
+		hash_tt->array[i] = NULL;
+	return (hash_tt);
 }

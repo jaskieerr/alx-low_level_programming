9161c19b2e575f28568b39f3f5a6d0b5f158e427
jaskieerr <nizar.zairat@gmail.com>
2023-11-03 16:30:32 -0700

    weeeedonthavedannnncceeeee

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - fetches the value
+ * @ht: hash t  ptr
+ * @key: hash key
+ * Return: the value fetched
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int key_idx;
+	hash_node_t *temp;
+
+	if (ht == NULL)
+		return (NULL);
+
+	if (key == NULL || *key == '\0')
+		return (NULL);
+
+	key_idx = key_index((unsigned char *)key, ht->size);
+
+	temp = ht->array[key_idx];
+
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
+
+	return (NULL);
+}

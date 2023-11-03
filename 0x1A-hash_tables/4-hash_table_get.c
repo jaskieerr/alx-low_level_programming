0a1db60a29748f01862800e3768a82af03278ef7
jaskieerr <nizar.zairat@gmail.com>
2023-11-03 16:37:04 -0700

    weeeedonthavedannnncceeeee

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,22 +11,16 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int key_idx;
 	hash_node_t *temp;
 
-	if (ht == NULL)
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+	    key == NULL || strlen(key) == 0)
 		return (NULL);
-
-	if (key == NULL || *key == '\0')
-		return (NULL);
-
-	key_idx = key_index((unsigned char *)key, ht->size);
-
+	key_idx = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[key_idx];
-
 	while (temp != NULL)
 	{
 		if (strcmp(temp->key, key) == 0)
 			return (temp->value);
 		temp = temp->next;
 	}
-
 	return (NULL);
 }

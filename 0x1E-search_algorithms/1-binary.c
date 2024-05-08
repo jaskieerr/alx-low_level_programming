e1ca94a1b62a71246f78f2e05931d7861bde6846
jaskieerr <nizar.zairat@gmail.com>
2024-05-08 05:15:27 -0700

    pottery

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,6 +13,7 @@ int binary_search(int *array, size_t size, int value)
 	size_t right_index = size - 1;
 	size_t mid_index;
 	size_t i;
+
 	if (array == NULL)
 		return (-1);
 	while (left_index <= right_index)
